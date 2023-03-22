35e493f64fc12e946460a2ab5c5d613517ebfdcf
Yaroslav Odintsov <yaroslavod@gmail.com>
2023-03-22 03:41:50 -0400

    modified:   cypress/e2e/job_cafe_api_post.cy.js new file:   cypress/fixtures/params.json

--- cypress/e2e/job_cafe_api_post.cy.js
+++ cypress/e2e/job_cafe_api_post.cy.js
@@ -1,19 +1,19 @@
 /// <reference types = "Cypress"/>
-
+import { data } from '..//fixtures/params.json'
 
 
 describe('Get Jobs Test', () => {
 
     let positionBody = {
-        "position": "QA",
-        "company": "LegionQAYaroslav",
-        "location": "Tel Aviv-Yafo, Israel",
+        "position": "QA9",
+        "company": "LegionQAYaroslav9",
+        "location": "Tel Aviv-Yafo, Israel9",
         "seniority": "junior",
         "link": "www.linkedin.com",
         "description": "some text",
         "time": "two hours ago",
         "salary": "100k",
-        "date": "2020-06-06T12:00:00"
+        "date": "2013-06-06T12:00:00"
     }
 
     let adminKey = 'adminadmin'
@@ -30,10 +30,32 @@ describe('Get Jobs Test', () => {
             console.log(Response.body)
             id = Response.body.id
             expect(Response.status).eq(201)
-            expect(Response.body.company).eq('LegionQAYaroslav')
+            expect(Response.body.company).eq('LegionQAYaroslav9')
 
 
 
+        })
+
+    })
+
+    it('create job listing test from fixture', () => {
+
+        data.forEach(element => {
+// vzytie data iz fixture i podstavlenie ix
+            cy.request({
+                method: 'POST',
+                url: '/create',
+                body: element,
+                qs: { key: adminKey }
+            }).then((Response) => {
+                console.log(Response.body)
+                id = Response.body.id
+                expect(Response.status).eq(201)
+                expect(Response.body.company).eq(element.company)
+                cy.deletePositionById(id)
+
+
+            })
         })
 
     })
